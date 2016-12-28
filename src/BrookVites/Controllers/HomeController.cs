b2a6dd1ffd3373981a3160a71d85e430f9fc7a7d
Pon ArunKumar Ramalingam <ponarunkumar@gmail.com>
2016-12-27 19:09:31 -0500

    Initial Commit

--- src/BrookVites/Controllers/HomeController.cs
+++ src/BrookVites/Controllers/HomeController.cs
@@ -3,33 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BrookVites.Models;
 
 namespace BrookVites.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning!" : "Good Afternoon!";
+            return View("myView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            ViewData["Message"] = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else
+            {
+                //there is a validation error
+                return View();
+            }
 
-            return View();
         }
 
-        public IActionResult Contact()
+        public ViewResult ListResponses()
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
